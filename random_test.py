5c55690c57b94f60b87044916692a8ddb939f880
yigci <35665116+yigci@users.noreply.github.com>
2019-03-31 13:57:58 +0300

    update
    deleted unnecessary files

---
+++ random_test.py
@@ -0,0 +1,67 @@
+import cv2
+import imutils
+import numpy as np
+
+
+def visual():
+    cv2.namedWindow("Output")
+    cap = cv2.VideoCapture(0)
+    while 1:
+        ret, image = cap.read()
+        cv2.waitKey(1)
+        res_y = int(len(image[0]))
+        res_x = int(len(image))
+        starty = int((res_y - res_x) / 2)
+        image = image[:, starty:starty + res_x]
+        origin = int(res_x / 2)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 16)
+        edged = cv2.Canny(gray, 50, 100)
+        kernel = np.ones((9, 9), np.uint8)
+        closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+        cnts = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        mycnts = []
+        for cnt in cnts:
+            if 7000 < cv2.contourArea(cnt) < 20000:
+                perimeter = cv2.arcLength(cnt, True)
+                approx = cv2.approxPolyDP(cnt, 0.12 * perimeter, True)
+                if len(approx) == 4:
+                    mycnts.append(cnt)
+
+        cv2.circle(image, (origin, origin), 1, (255, 255, 255), -1)
+        for cnt in mycnts:
+            data = []
+            if 6500 < cv2.contourArea(cnt) < 21000:
+                rect = cv2.minAreaRect(cnt)
+                box = cv2.boxPoints(rect)
+                box = list(box[1])
+                print(box[1])
+
+                #               box = cv2.boxPoints(rect)
+                #               box = np.int0(box)
+                #               cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
+                rect = list(rect)
+                angle = list(np.float_(rect[2:3]))
+                angle = float(angle[0])
+                coor = list(np.float_(rect[0:1]))
+                coor = coor[0]
+                cx = int(coor[0])
+                cy = int(coor[1])
+                cv2.circle(image, (cx, cy), 1, (0, 0, 255), -1)
+                cv2.putText(image, "center: %f-%f  Angle: %f" % (coor[0], coor[1], angle), (15, 15),
+                            cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 1)
+
+                data.append((coor[0]))
+                data.append((coor[1]))
+                data.append(angle)
+                cv2.drawContours(image, mycnts, -1, (0, 255, 0), 1)
+                print(data[2])
+
+        image[origin, origin - 5:origin + 5] = (255, 0, 0)
+        image[origin - 5:origin + 5, origin] = (255, 0, 0)
+        cv2.imshow("Output", image)
+
+
+
+visual()
